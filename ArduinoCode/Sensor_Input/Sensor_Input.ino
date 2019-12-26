19c841865c08d0c5cd984afccf7edbe3dce60282
Spencer Barnes <interstellarjumper@gmail.com>
2019-12-26 16:23:59 -0600

    Pulled helper functions out to helper libraries

--- ArduinoCode/Sensor_Input/Sensor_Input.ino
+++ ArduinoCode/Sensor_Input/Sensor_Input.ino
@@ -1,73 +1,16 @@
 #include <Arduino.h>
 #include <NewPing.h>
-//DEBUG Library: Used for measuring Heap Fragmentation
-#include "MemoryInfo.h"
+#include "JsonStringBuilder.h"
+
+//DEBUG Library: For debug testing and performance testing
+#include "DebugAndPerformanceTest.h"
 
 #define SONAR1_trig 12
 #define SONAR1_echo 11
 #define LIMITSWITCH1 8
 
 NewPing sonar(SONAR1_trig, SONAR1_echo);
-
-//DEBUG Variables: Dummy variables for timer performance and testing logs
-unsigned dummyData = 0;
-unsigned long avgDummyTime = 0;
-unsigned long dummyStart = 0;
-
 String incoming;
-
-// Class to simplify creating JSON strings 
-class JsonStringBuilder
-{
-  private:
-    String message;
-
-  public:
-    // I wanted default parameters of 1 then 10, but Arduino can ONLY have defaults in the
-    //  prototype, not in the defintion. When this is moved to a library, define defaults
-    //  in the header file's prototypes
-    JsonStringBuilder(int estimatedNumberOfElements, int estimatedElementSize)
-    {
-      message = "";
-      message.reserve((estimatedElementSize+4)*estimatedNumberOfElements);
-    }
-
-    // Overload for string data, needs quotes \"
-    void add(String propertyName, String propertyValue)
-    {
-      message += "\""+ propertyName +"\":\""+ propertyValue +"\",";
-    }
-
-    // Overload for other JSON builders, uses getJsonString and no quotes
-    void add(String propertyName, JsonStringBuilder propertyValue)
-    {
-      message += "\""+ propertyName +"\":"+ propertyValue.getJsonString() +",";
-    }
-
-    // Overload for bool data, write as true/false not 1/0
-    void add(String propertyName, bool propertyValue)
-    {
-      message += "\""+ propertyName +"\":"+ String(propertyValue?"true":"false") +",";
-    }
-
-    template <class T>
-    void add(String propertyName, T propertyValue)
-    {
-      // Name requires quotes, thus they must be escaped as \"
-      message += "\""+ propertyName +"\":"+ propertyValue +",";
-    }
-
-    String getJsonString()
-    {
-      // Cut off last comma , and encompass in braces {}
-      return "{"+ message.substring(0, message.length()-1) +"}";
-    }
-
-    bool empty()
-    {
-      return message == "";
-    }
-};
 
 void setup() 
 {
@@ -82,13 +25,7 @@ void setup()
 
 void loop() 
 {
-  if (dummyData >= 60000)
-  {
-    avgDummyTime = ((avgDummyTime*3) + (millis() - dummyStart))/4;
-    dummyData = 0;
-    dummyStart = millis();
-  }
-  dummyData++;
+  testCode();
 }
 
 // Get Sonar data in json style string
@@ -124,24 +61,6 @@ void getSensorData(JsonStringBuilder &outgoing)
   //Limit switch sensors:
   // LIMITSWITCH1
   outgoing.add("limitSwitch1", getLimitSwitchData(LIMITSWITCH1));
-}
-
-//DEBUG Function: Get various performance statistics
-void getPerformanceData(JsonStringBuilder &outgoing)
-{
-  // Show avg time to count dummyData
-  outgoing.add("avgDummyTime", "{\"data\":"+String(avgDummyTime)+",\"units\":\"ms\"}");
-  // Show heap fragmentation
-  outgoing.add("Heap Fragmentation", "{\"data\":"+String(getFragmentation())+",\"units\":\"%\"}");
-}
-
-//DEBUG Function: Get data used just for testing puroses
-void getTestData(JsonStringBuilder &outgoing)
-{
-  outgoing.add("tick", String("."));
-  if (dummyData % 50 == 0) {
-    outgoing.add("Dumb Chance", String(dummyData));
-  }
 }
 
 // Serial input parser
